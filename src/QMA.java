b0b8deaf36019fffbdb92ba1b3504865b1db76bd
aryam9 <aryamurugan007@gmail.com>
2026-04-20 09:53:44 +0530

    UC2 - Feet and Inches measurement equality

--- src/QMA.java
+++ src/QMA.java
@@ -28,64 +28,104 @@ class Feet {
     }
 }
 
+class Inch {
+
+    private final double value;
+
+    public Inch(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Inch other = (Inch) obj;
+
+        return Double.compare(this.value, other.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(value);
+    }
+}
+
 class QuantityMeasurementTests {
 
-    static void testEquality_SameValue() {
+    // FEET TESTS
+    static void testFeet_SameValue() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
 
         System.out.println(
-                "Same Value Test -> " +
-                        f1.getValue() + " ft and " +
+                "Feet Same Value -> " +
+                        f1.getValue() + " ft vs " +
                         f2.getValue() + " ft => " +
                         f1.equals(f2)
         );
     }
 
-    static void testEquality_DifferentValue() {
+    static void testFeet_DifferentValue() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(2.0);
 
         System.out.println(
-                "Different Value Test -> " +
-                        f1.getValue() + " ft and " +
+                "Feet Different Value -> " +
+                        f1.getValue() + " ft vs " +
                         f2.getValue() + " ft => " +
                         f1.equals(f2)
         );
     }
 
-    static void testEquality_NullComparison() {
-        Feet f1 = new Feet(1.0);
+    // INCH TESTS
+    static void testInch_SameValue() {
+        Inch i1 = new Inch(1.0);
+        Inch i2 = new Inch(1.0);
 
         System.out.println(
-                "Null Comparison Test -> " +
-                        f1.getValue() + " ft and null => " +
-                        f1.equals(null)
+                "Inch Same Value -> " +
+                        i1.getValue() + " in vs " +
+                        i2.getValue() + " in => " +
+                        i1.equals(i2)
         );
     }
 
-    static void testEquality_SameReference() {
-        Feet f1 = new Feet(1.0);
+    static void testInch_DifferentValue() {
+        Inch i1 = new Inch(1.0);
+        Inch i2 = new Inch(2.0);
 
         System.out.println(
-                "Same Reference Test -> " +
-                        f1.getValue() + " ft and itself => " +
-                        f1.equals(f1)
+                "Inch Different Value -> " +
+                        i1.getValue() + " in vs " +
+                        i2.getValue() + " in => " +
+                        i1.equals(i2)
         );
     }
 
     static void runAllTests() {
-        testEquality_SameValue();
-        testEquality_DifferentValue();
-        testEquality_NullComparison();
-        testEquality_SameReference();
+        System.out.println("===== FEET TESTS =====");
+        testFeet_SameValue();
+        testFeet_DifferentValue();
+
+        System.out.println("\n===== INCH TESTS =====");
+        testInch_SameValue();
+        testInch_DifferentValue();
     }
 }
 
 public class QMA {
+
     public static void main(String[] args) {
 
-        System.out.println("===== Quantity Measurement Test Run =====\n");
+        System.out.println("===== Quantity Measurement App =====\n");
 
         QuantityMeasurementTests.runAllTests();
     }
